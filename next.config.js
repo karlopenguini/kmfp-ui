1051ac3895902fa52493d7a8009ed8e5075d5d34
Karlo Miguel Palisoc <karobpalisoc@gmail.com>
2023-06-25 08:28:57 +0800

    test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
+  images: {
         remotePatterns: [
           {
-            
             hostname: 'images.unsplash.com',
             
           },
@@ -15,7 +14,37 @@ const nextConfig = {
             hostname: 'xchyxsks.api.sanity.io'
           }
         ],
-      },
+  },
+
+  async headers() {
+
+    return [
+
+      {
+
+        // matching all API routes
+
+        source: "/api/:path*",
+
+        headers: [
+
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+
+          { key: "Access-Control-Allow-Origin", value: "images.unsplash.com" },
+          { key: "Access-Control-Allow-Origin", value: "cdn.sanity.io" },
+          { key: "Access-Control-Allow-Origin", value: "xchyxsks.api.sanity.io" },
+
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+
+        ]
+
+      }
+
+    ]
+
+  },
 }
 
 module.exports = nextConfig
