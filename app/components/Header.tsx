968ed7955cbfe3132509898a29f21193536135fa
Karlo Miguel Palisoc <karobpalisoc@gmail.com>
2023-06-26 14:22:12 +0800

    ehhh

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,12 +4,9 @@ import Wave from "./svg/Wave";
 const Header = () => {
   return (
     <div className="w-full flex justify-between">
-      <div className="max-md:mx-auto scale-75">
-        <Logo />
-      </div>
-      <div className="max-md:hidden">
-        <Wave />
-      </div>
+      <Logo />
+
+      <Wave />
     </div>
   );
 };
